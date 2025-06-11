30d2532a91f14dcbb47020cf95ef725a27d9c633
b_g <162826027+1408bg@users.noreply.github.com>
2025-06-11 15:13:25 +0900

    Update problems/전화번호-뒷자리-구하기/problem.js

---
+++ problems/전화번호-뒷자리-구하기/problem.js
@@ -0,0 +1,16 @@
+const { start, add, end, answer } = context;
+
+start();
+
+const s = answer();
+
+const numbers = [
+  '010-1234-5678',
+  '010-1234-5678'
+];
+
+numbers.forEach(number => {
+  add(number, s(number) === number.split('-').at(-1));
+});
+
+end();
